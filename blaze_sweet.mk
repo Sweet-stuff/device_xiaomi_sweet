3c30d17b4049dc5170c29c7cfcd0692f3e766e76
kangeraarav <okand7946@gmail.com>
2022-11-27 09:09:39 +0530

    Sweet: Project Blaze Bringup [By AARAV&ADITYA]

--- blaze_sweet.mk
+++ blaze_sweet.mk
@@ -11,13 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := blaze_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Inherit some common Project Blaze flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+BLAZE_MAINTAINER := Aarav&Aditya
+TARGET_USE_PIXEL_CHARGER := true
+BLAZE_BUILD_TYPE := OFFICIAL
